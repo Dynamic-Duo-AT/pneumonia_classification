2fae91874f307e1798bd302a403e358391c984cb
AndreasRahbekPalm <andreas.r.p@hotmail.com>
2026-01-21 11:10:09 +0100

    merge test_api.py into main and reformatted with ruff

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,16 +1,19 @@
 from fastapi.testclient import TestClient
 from pneumonia.api import app
 
+
 def test_read_root():
     with TestClient(app) as client:
         response = client.get("/")
         assert response.status_code == 200
-        assert response.json() == {"Welcome":"To the Pneumonia Detection API"}
+        assert response.json() == {"Welcome": "To the Pneumonia Detection API"}
+
 
 def test_pred_empty_upload():
     with TestClient(app) as client:
         response = client.post("/pred/", files={"data": ("", b"")})
         assert response.status_code == 422
+
 
 def test_pred_valid_image():
     with TestClient(app) as client:
@@ -19,7 +22,7 @@ def test_pred_valid_image():
             resp = client.post(
                 "/pred/",
                 files={"data": ("IM-0119-0001.jpeg", f, "image/jpeg")},
-                headers={"accept": "application/json"},  
+                headers={"accept": "application/json"},
             )
         assert resp.status_code == 200
         json_response = resp.json()
